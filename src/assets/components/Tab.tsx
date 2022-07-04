c655444f315c0a042a91ee4ea4f5675b328f81ed
Kévin Lacroix <kevin.lacroix@hetic.net>
2022-07-05 01:23:51 +0200

    Update design system

--- src/assets/components/Tab.tsx
+++ src/assets/components/Tab.tsx
@@ -8,12 +8,12 @@ export default function Tab(props:any) {
   return (
     <div className="container-tab">
         <div className='tab'>
-        <a className={` ${tabIndex === 0 ? 'tab-active' : 'tab-default'} `}
+        <a className={` ${tabIndex === 0 ? 'tab__link-active' : 'tab__link'} `}
             onClick={ () => { setTabIndex(0) } }
             href="#">{props.left_title}
         </a>
 
-        <a className={` ${tabIndex === 1 ? 'tab-active' : 'tab-default'} `}
+        <a className={` ${tabIndex === 1 ? 'tab__link-active' : 'tab__link'} `}
             onClick={ () => { setTabIndex(1) } }
             href="#">{props.right_title}
         </a>
